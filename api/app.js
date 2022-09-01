93f43e954b35bd4c3e9b8078e11ac55726c0abf2
Tomas Di Bacco <100334543+Tdibacco17@users.noreply.github.com>
2022-09-01 10:38:51 -0300

    falta cors

--- api/app.js
+++ api/app.js
@@ -12,9 +12,7 @@ const app = express();
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(cookieParser());
-app.use(cors({
-  origin: "*"
-}));
+app.use(cors());
 
 app.use("/", CharacterRoutes)
 
