5415398d0accb2ca8b38e7d5d317b75c9cc153a6
Tomas Di Bacco <100334543+Tdibacco17@users.noreply.github.com>
2022-09-19 14:36:57 -0300

    cors

--- api/app.js
+++ api/app.js
@@ -10,7 +10,6 @@ require('./db.js');
 const app = express();
 
 app.use(cors());
-app.setHeader("Access-Control-Allow-Origin", "*");
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(cookieParser());
