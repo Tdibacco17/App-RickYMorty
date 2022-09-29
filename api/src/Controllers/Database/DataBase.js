3af9f26cd856c8531ff9a1fd9b3efd77bca7e4d3
Tomas Di Bacco <100334543+Tdibacco17@users.noreply.github.com>
2022-09-29 11:44:52 -0300

    cors -_-

--- api/src/Controllers/Database/DataBase.js
+++ api/src/Controllers/Database/DataBase.js
@@ -1,58 +1,58 @@
-// const { getAllCharacters, getAllEpisodes, getAllLocations } = require("./FunctionController");
-// const { Character, Episode, Location } = require('../../../db');
+const { getAllCharacters, getAllEpisodes, getAllLocations } = require("./FunctionController");
+const { Character, Episode, Location } = require('../../../db');
 
 
-// const characterDataBase = async (req, res) => {
+const characterDataBase = async (req, res) => {
 
-//     try {
-//         const personajes = await getAllCharacters();
+    try {
+        const personajes = await getAllCharacters();
 
-//         const result1 = personajes.map(e => e.map(j => {
-//             return Character.create({
-//                 characterId: j.characterId,
-//                 name: j.name,
-//                 status: j.status,
-//                 species: j.species,
-//                 gender: j.gender,
-//                 origin: { id_location: j.origin.id_location.slice(41) },
-//                 location: { id_location: j.location.id_location.slice(41) },
-//                 episode: j.episode,
-//                 image: j.image,
-//                 createdDay: j.createdDay,
-//                 createdTime: j.createdTime,
-//             })
-//         }))
+        const result1 = personajes.map(e => e.map(j => {
+            return Character.create({
+                characterId: j.characterId,
+                name: j.name,
+                status: j.status,
+                species: j.species,
+                gender: j.gender,
+                origin: { id_location: j.origin.id_location.slice(41) },
+                location: { id_location: j.location.id_location.slice(41) },
+                episode: j.episode,
+                image: j.image,
+                createdDay: j.createdDay,
+                createdTime: j.createdTime,
+            })
+        }))
 
-//         const episodios = await getAllEpisodes();
+        const episodios = await getAllEpisodes();
 
-//         const result2 = episodios.map(e => e.map(j => {
-//             return Episode.create({
-//                 episodeId: j.episodeId,
-//                 name: j.name,
-//                 air_date: j.air_date,
-//                 temporada: j.temporada,
-//                 capitulo: j.capitulo,
-//                 characters: j.characters,
-//             })
-//         }))
+        const result2 = episodios.map(e => e.map(j => {
+            return Episode.create({
+                episodeId: j.episodeId,
+                name: j.name,
+                air_date: j.air_date,
+                temporada: j.temporada,
+                capitulo: j.capitulo,
+                characters: j.characters,
+            })
+        }))
 
 
-//         const locaciones = await getAllLocations();
+        const locaciones = await getAllLocations();
 
-//         const result3 = locaciones.map(e => e.map(j => {
-//             return Location.create({
-//                 locationId: j.locationId,
-//                 name: j.name,
-//                 type: j.type,
-//                 dimension: j.dimension,
-//                 residents: j.residents,
-//             })
-//         }))
+        const result3 = locaciones.map(e => e.map(j => {
+            return Location.create({
+                locationId: j.locationId,
+                name: j.name,
+                type: j.type,
+                dimension: j.dimension,
+                residents: j.residents,
+            })
+        }))
 
-//         return res.json("Base de datos cargada")
-//     } catch (e) {
-//         return res.status(400).json({ msg: `Error 404 - ${e}` });
-//     }
-// }
+        return res.json("Base de datos cargada")
+    } catch (e) {
+        return res.status(400).json({ msg: `Error 404 - ${e}` });
+    }
+}
 
-// module.exports = { characterDataBase };
+module.exports = { characterDataBase };
