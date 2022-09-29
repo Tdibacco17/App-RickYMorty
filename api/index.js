4253da0561dda573b799d83c5407dec5782cff8a
Tomas Di Bacco <100334543+Tdibacco17@users.noreply.github.com>
2022-09-29 11:36:12 -0300

    deploy

--- api/index.js
+++ api/index.js
@@ -7,7 +7,7 @@ const { db } = require("./db");
 const PORT = process.env.PORT || 3000;
 const HOST = process.env.HOST || "0.0.0.0";
 
-db.sync({ force: false }).then(() => {
+db.sync({ force: true }).then(() => {
   console.log("Base de datos conectada!");
   app.listen(PORT, HOST, () => {
     console.log(`Listening in ${PORT}/`); // eslint-disable-line no-console
