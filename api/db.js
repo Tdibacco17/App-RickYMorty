fff28220bf0591bbbbd6a058845a794fee3ed31d
Tdibacco17 <tomas.dibacco@25watts.com.ar>
2022-11-16 17:20:33 -0300

    database en railway

--- api/db.js
+++ api/db.js
@@ -4,43 +4,16 @@ const { Sequelize } = require("sequelize");
 const modelCharacter = require("./src/Models/Character");
 const modelEpisode = require("./src/Models/Episode");
 const modelLocation = require("./src/Models/Location")
-// const { pg } = require('pg');
+const { pg } = require('pg');
 
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
-// const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, {
-//   logging: false,
-//   native: false,
-//   dialectModule: pg
-// });
+const { PGUSER, PGPASSWORD, PGHOST, PGPORT, PGDATABASE } = process.env;
 
-let sequelize =
-  process.env.NODE_ENV === "production"
-    ? new Sequelize({
-        database: DB_NAME,
-        dialect: "postgres",
-        host: DB_HOST,
-        port: 5432,
-        username: DB_USER,
-        password: DB_PASSWORD,
-        pool: {
-          max: 3,
-          min: 1,
-          idle: 10000,
-        },
-        dialectOptions: {
-          ssl: {
-            require: true,
-            // Ref.: https://github.com/brianc/node-postgres/issues/2009
-            rejectUnauthorized: false,
-          },
-          keepAlive: true,
-        },
-        ssl: true,
-      })
-    : new Sequelize(
-        `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
-        { logging: false, native: false }
-      );
+const sequelize = new Sequelize(`postgresql://${ PGUSER }:${ PGPASSWORD }@${ PGHOST }:${ PGPORT }/${PGDATABASE }`, {
+  logging: false, // set to console.log to see the raw SQL queries
+  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+  dialectModule: pg
+});
+
 
 modelCharacter(sequelize);
 modelEpisode(sequelize);
--- api/index.js
+++ api/index.js
@@ -4,12 +4,12 @@ const { db } = require("./db");
 // const PORT = process.env.PGPORT || 6031;
 // const HOST = process.env.HOST || "0.0.0.0";
 
-const PORT = process.env.PORT || 3000;
-const HOST = process.env.HOST || "0.0.0.0";
+const PORT = process.env.PORT || 6032;
+// const HOST = process.env.HOST || "0.0.0.0";
 
 db.sync({ force: false }).then(() => {
   console.log("Base de datos conectada!");
-  app.listen(PORT, HOST, () => {
+  app.listen(PORT, () => {
     console.log(`Listening in ${PORT}/`); // eslint-disable-line no-console
   });
 });
