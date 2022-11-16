7dc153d4eda4196d10276e062393b737d1874208
Tdibacco17 <tomas.dibacco@25watts.com.ar>
2022-11-16 17:35:51 -0300

    prueba vercel back

--- api/app.js
+++ api/app.js
@@ -17,12 +17,4 @@ app.use(cookieParser());
 
 app.use("/", CharacterRoutes)
 
-// Error catching endware.
-app.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
-  const status = err.status || 500;
-  const message = err.message || err;
-  console.error(err);
-  res.status(status).send(message);
-});
-
 module.exports = app;
