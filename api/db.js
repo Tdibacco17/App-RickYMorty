28e72f9d0c9ab633648aef85fb168ab6c432d04a
Tomas Di Bacco <100334543+Tdibacco17@users.noreply.github.com>
2023-01-13 19:20:57 -0300

    commit 2

--- api/db.js
+++ api/db.js
@@ -8,42 +8,42 @@ const { pg } = require('pg');
 
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME,
   PGUSER, PGPASSWORD, PGHOST, PGPORT, PGDATABASE } = process.env;
-// const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, {
-//   logging: false,
-//   native: false,
-//   dialectModule: pg
-// });
-postgresql://${{ PGUSER }}:${{ PGPASSWORD }}@${{ PGHOST }}:${{ PGPORT }}/${{ PGDATABASE }}
+const sequelize = new Sequelize(`postgresql://${{ PGUSER }}:${{ PGPASSWORD }}@${{ PGHOST }}:${{ PGPORT }}/${{ PGDATABASE }}`, {
+  logging: false,
+  native: false,
+  dialectModule: pg
+});
 
 
-let sequelize =
-  process.env.NODE_ENV === "production"
-    ? new Sequelize({
-      database: DB_NAME,
-      dialect: "postgres",
-      host: DB_HOST,
-      port: 5432,
-      username: DB_USER,
-      password: DB_PASSWORD,
-      pool: {
-        max: 3,
-        min: 1,
-        idle: 10000,
-      },
-      dialectOptions: {
-        ssl: {
-          require: true,
-          // Ref.: https://github.com/brianc/node-postgres/issues/2009
-          rejectUnauthorized: false,
-        },
-        keepAlive: true,
-      },
-      ssl: true,
-    })
-    : new Sequelize(
-      `postgres://${{ PGUSER }}:${{ PGPASSWORD }}@${{ PGHOST }}:${{ PGPORT }}/${{ PGDATABASE }}`,
-      { logging: false, native: false }
-    );
+
+// let sequelize =
+//   process.env.NODE_ENV === "production"
+//     ? new Sequelize({
+//       database: DB_NAME,
+//       dialect: "postgres",
+//       host: DB_HOST,
+//       port: 5432,
+//       username: DB_USER,
+//       password: DB_PASSWORD,
+//       pool: {
+//         max: 3,
+//         min: 1,
+//         idle: 10000,
+//       },
+//       dialectOptions: {
+//         ssl: {
+//           require: true,
+//           // Ref.: https://github.com/brianc/node-postgres/issues/2009
+//           rejectUnauthorized: false,
+//         },
+//         keepAlive: true,
+//       },
+//       ssl: true,
+//     })
+//     : new Sequelize(
+//       `postgres://${{ PGUSER }}:${{ PGPASSWORD }}@${{ PGHOST }}:${{ PGPORT }}/${{ PGDATABASE }}`,
+//       { logging: false, native: false }
+//     );
 
 modelCharacter(sequelize);
 modelEpisode(sequelize);
