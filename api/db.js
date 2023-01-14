c4da80ffd539bddb5c45cf78a822d5fb72aa5f43
Tomas Di Bacco <100334543+Tdibacco17@users.noreply.github.com>
2023-01-13 22:18:53 -0300

    commit

--- api/db.js
+++ api/db.js
@@ -6,14 +6,44 @@ const modelEpisode = require("./src/Models/Episode");
 const modelLocation = require("./src/Models/Location")
 const { pg } = require('pg');
 
-const { PGUSER, PGPASSWORD, PGHOST, PGPORT, PGDATABASE } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME,
+  PGUSER, PGPASSWORD, PGHOST, PGPORT, PGDATABASE } = process.env;
+// const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, {
+//   logging: false,
+//   native: false,
+//   dialectModule: pg
+// });
+postgresql://${{ PGUSER }}:${{ PGPASSWORD }}@${{ PGHOST }}:${{ PGPORT }}/${{ PGDATABASE }}
 
-const sequelize = new Sequelize(`postgresql://${ PGUSER }:${ PGPASSWORD }@${ PGHOST }:${ PGPORT }/${PGDATABASE }`, {
-  logging: false, // set to console.log to see the raw SQL queries
-  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-  dialectModule: pg
-});
 
+let sequelize =
+  process.env.NODE_ENV === "production"
+    ? new Sequelize({
+      database: DB_NAME,
+      dialect: "postgres",
+      host: DB_HOST,
+      port: 5432,
+      username: DB_USER,
+      password: DB_PASSWORD,
+      pool: {
+        max: 3,
+        min: 1,
+        idle: 10000,
+      },
+      dialectOptions: {
+        ssl: {
+          require: true,
+          // Ref.: https://github.com/brianc/node-postgres/issues/2009
+          rejectUnauthorized: false,
+        },
+        keepAlive: true,
+      },
+      ssl: true,
+    })
+    : new Sequelize(
+      `postgres://${{ PGUSER }}:${{ PGPASSWORD }}@${{ PGHOST }}:${{ PGPORT }}/${{ PGDATABASE }}`,
+      { logging: false, native: false }
+    );
 
 modelCharacter(sequelize);
 modelEpisode(sequelize);
