76fa64927e2c8bf893a7133f075f4dace760af93
Tomas Di Bacco <100334543+Tdibacco17@users.noreply.github.com>
2023-01-25 11:35:29 -0300

    Update Footer.js

--- cliente/src/Components/Footer/Footer.js
+++ cliente/src/Components/Footer/Footer.js
@@ -12,7 +12,7 @@ export default function Footer() {
             <div>
                 <a href="mailto:tomasdibacco@gmail.com" rel="noopener noreferrer" target="_blank"><SiGmail className={darkMode === true ? "iconosFooter" : "iconosFooterDark" } /></a>
                 <a href="https://github.com/Tdibacco17" rel="noopener noreferrer" target="_blank"><SiGithub className={darkMode === true ? "iconosFooter" : "iconosFooterDark" }/></a>
-                <a href="https://www.linkedin.com/in/tomas-di-bacco-20a7a7240/" rel="noopener noreferrer" target="_blank"><SiLinkedin className={darkMode === true ? "iconosFooter" : "iconosFooterDark" }/></a>
+                <a href="https://www.linkedin.com/in/tomas-di-bacco/" rel="noopener noreferrer" target="_blank"><SiLinkedin className={darkMode === true ? "iconosFooter" : "iconosFooterDark" }/></a>
             </div>
             <div>
                 <h5 className={darkMode === true ? "tituloFooter" : "tituloFooterDark" }>By Tomas Di Bacco</h5>
